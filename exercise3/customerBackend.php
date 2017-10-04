4af6c1a5894bc5cac2113c9929fa2afa6161fd64
LiamOrmiston <ljormiston@ku.edu>
2017-10-04 03:08:33 -0500

    initialize cost

--- exercise3/customerBackend.php
+++ exercise3/customerBackend.php
@@ -6,4 +6,9 @@
   $monitor = $_POST["monitor"];
   $mouse = $_POST["mouse"];
   $keyboard = $_POST["keyboard"];
+
+  $ship_cost = 0;
+  $mon_total = number_format(($monitor*150),'2');
+  $mouse_total = number_format(($mouse*15),'2');
+  $key_total = number_format(($keyboard*75),'2');
 ?>
