ba91c32fbaf78e7e49f99e0c0b25bc9a6a559522
LiamOrmiston <ljormiston@ku.edu>
2017-10-04 00:03:39 -0500

    added first row and comments

--- exercise1/ex1.php
+++ exercise1/ex1.php
@@ -1,8 +1,14 @@
 <?php
   $total = 100;
   for ($i = 1; $i <= $total; $i++) {
+    // first row
+    echo $i;
+  }
+  for ($i = 1; $i <= $total; $i++) {
+    // first column
     echo $i;
     for ($j = 1; $j <= $total; $j++) {
+      // multiplication table content
       echo $i*$j;
     }
   }
