da4428ea7d64ebdb255da3731aac08b8a210d3a0
LiamOrmiston <ljormiston@ku.edu>
2017-10-04 10:17:01 -0500

    added table for ex1

--- exercise1/ex1.php
+++ exercise1/ex1.php
@@ -1,15 +1,20 @@
 <?php
   $total = 100;
+  echo '<table>';
+  echo '<tr><th></th>';
   for ($i = 1; $i <= $total; $i++) {
     // first row
-    echo $i;
+    echo "<th>" . $i . "</th>";
   }
+  echo '</tr>';
   for ($i = 1; $i <= $total; $i++) {
     // first column
-    echo $i;
+    echo '<tr><th>' . $i . '</th>';
     for ($j = 1; $j <= $total; $j++) {
       // multiplication table content
-      echo $i*$j;
+      echo "<td>". $i*$j . "</td>";
     }
+    echo "</tr>";
   }
+  echo '</table>';
 ?>
