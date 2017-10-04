39ee693520e78241d08560e28be9f90be5033ee4
LiamOrmiston <ljormiston@ku.edu>
2017-10-04 03:05:32 -0500

    initialize values

---
+++ exercise3/customerBackend.php
@@ -0,0 +1,9 @@
+<?php
+	echo '<link type="text/css" rel="stylesheet" href="style.css">';
+	$username = $_POST["username"];
+	$password = $_POST["password"];
+	$shipping = $_POST["shipping"];
+  $monitor = $_POST["monitor"];
+  $mouse = $_POST["mouse"];
+  $keyboard = $_POST["keyboard"];
+?>
