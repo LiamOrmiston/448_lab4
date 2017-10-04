242e4fa9bb37b7d1ece7e1a63760ea63f3ff6287
LiamOrmiston <ljormiston@ku.edu>
2017-10-04 02:52:10 -0500

    checks for negative values

--- exercise3/formChecker.js
+++ exercise3/formChecker.js
@@ -3,6 +3,10 @@ function check(form) {
 	var m_password = form.password.value;
 	var m_shipping = form.shipping.value;
 
+  var m_monitor = form.monitor.value;
+  var m_mouse = form.mouse.value;
+  var m_keyboard = form.keyboard.value;
+
   var m_error = false;
   var m_feedback = "";
 
@@ -17,5 +21,18 @@ function check(form) {
   if (!m_shipping) {
   m_error = true;
   m_feedback += "<p>Please pick a shipping option</p>"
+  }
+
+  if (m_monitor < 0) {
+  m_error = true;
+  m_feedback += "<p>Monitor quantity cannot be negative</p>"
+  }
+  if (m_mouse < 0) {
+  m_error = true;
+  m_feedback += "<p>Mouse quantity cannot be negative</p>"
+  }
+  if (m_keyboard < 0) {
+  m_error = true;
+  m_feedback += "<p>Keyboard quantity cannot be negative</p>"
   }
 }
